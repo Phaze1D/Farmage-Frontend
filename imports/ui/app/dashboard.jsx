7c7092be5b110aef641ac1f35a99f51af714d4b8
DavidMBP <steadypathapp@gmail.com>
2016-10-26 20:08:16 -0500

    Added packages for react-toolbox

--- imports/ui/app/dashboard.jsx
+++ imports/ui/app/dashboard.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-
+import Drawer from 'react-toolbox/lib/drawer';
+import Button from 'react-toolbox/lib/button';
 
 export default class Dashboard extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      active: false
+    };
+    this.drawerToggle = this.drawerToggle.bind(this);
   }
+
+  drawerToggle() {
+    this.setState({active: !this.state.active});
+  };
+
 
   render() {
     return (
-      <h1>Testing Dashboard</h1>
+      <div>
+        <Button label='Show Drawer' raised accent onClick={this.drawerToggle} />
+        <Drawer active={this.state.active} onOverlayClick={this.drawerToggle} theme={theme}>
+          <h5>This is your Drawer.</h5>
+        </Drawer>
+      </div>
+
     )
   }
 
