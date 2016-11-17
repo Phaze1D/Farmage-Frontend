0a9cf9e2f8557beb7f2e2ebac66a9e25de969ef3
DavidMBP <steadypathapp@gmail.com>
2016-11-17 16:17:49 -0600

    Change default image to Letter Avatar

--- imports/ui/models/person/PersonSelectorItem.jsx
+++ imports/ui/models/person/PersonSelectorItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Avatar from 'material-ui/Avatar';
+
 
 
 
@@ -7,7 +9,7 @@ let PersonSelectorItem = (props) => (
     <div className='col-xs-12'>
       <div className='selector-item'>
         <div className='select-title'>
-          <div className='img-div' style={{backgroundImage: 'url(/default_avatar.jpg)'}}></div>
+          <Avatar style={{marginRight: '15px', padding: '2px 0 0 2px'}} size={56}>F</Avatar>
 
           <h3>
             First Lastname
