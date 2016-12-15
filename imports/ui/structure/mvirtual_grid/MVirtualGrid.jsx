886e1e29b7a8dde62b82efd37de55d7adddb8a07
DavidMBP <steadypathapp@gmail.com>
2016-12-15 16:00:46 -0600

    Added MVirtual Grid

---
+++ imports/ui/structure/mvirtual_grid/MVirtualGrid.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
+
+export default class MVirtualGrid extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <ReactCSSTransitionGroup
+        transitionName={ {
+          enter: 'enter-index',
+          leave: 'leave-index',
+          appear: 'appear-index'
+        } }
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+        transitionAppear={true}
+        transitionAppearTimeout={400}>
+
+        <div className='row is-flex'>
+          {this.props.children}
+        </div>
+      </ReactCSSTransitionGroup>
+    )
+  }
+}
