3da5e00043458eba3b2015b1224d6a79614e04ca
DavidMBP <steadypathapp@gmail.com>
2016-12-24 15:13:17 -0600

    Fixed first scroll issue

--- imports/ui/structure/main_panel/MainPanel.jsx
+++ imports/ui/structure/main_panel/MainPanel.jsx
@@ -64,7 +64,7 @@ export default class MainPanel extends React.Component{
         />
 
         <EventListener
-          target={this.props.targetScroll}
+          target='panel'
           onScroll={this.onScroll}
         />
 
@@ -74,7 +74,7 @@ export default class MainPanel extends React.Component{
             {this.props.title}
           </div>
         </div>
-        <div className={'panel '+this.props.classes}>
+        <div id='panel' className={'panel '+this.props.classes}>
           {this.props.children}
         </div>
       </div>
