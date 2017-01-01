b7034f5689cae932a8d12ba948a641d9b0c4bc3f
DavidMBP <steadypathapp@gmail.com>
2016-12-31 18:35:49 -0600

    Added requestanin

--- imports/ui/structure/main_panel/MainPanel.jsx
+++ imports/ui/structure/main_panel/MainPanel.jsx
@@ -9,42 +9,55 @@ export default class MainPanel extends React.Component{
   constructor(props){
     super(props);
     this.bigHeaderHidden = false;
+    this.ticking = false;
 
     this.handleScroll = this.handleScroll.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  componentDidMount() {
+    this.title = document.getElementById(`${this.props.panelID}-ttbar`);
+    this.toolbar = this.title.closest('.toolbar');
   }
 
   handleScroll(event){
-    let scrollTop = event.currentTarget.scrollTop;
-    let title = document.getElementById(`${this.props.panelID}-ttbar`);
-    let toolbar = title.closest('.toolbar');
-    let f = 212 - toolbar.clientHeight;
+    let last_known_scroll_position = event.currentTarget.scrollTop;
+    if (!this.ticking) {
+        requestAnimationFrame(() => {
+          this.update(last_known_scroll_position);
+          this.ticking = false;
+        });
+      }
+      this.ticking = true;
+  }
 
-    if(scrollTop < f ){
+  update(lastScrollY){
+    let f = 212 - this.toolbar.clientHeight;
+
+    if(lastScrollY < f ){
 
       if(this.bigHeaderHidden){
-        toolbar.style.boxShadow = ``
+        this.toolbar.style.boxShadow = ``
       }
-      let scale = - (scrollTop / f ) + 2;
-      let y = 54 * (1 - scrollTop / f);
-      let a = toolbar.clientWidth > 840 ? 40 : 15;
-      let x = a * (1 - scrollTop / f);
-      title.style.transform = `scale(${scale}) translate(${x}%, ${y}px)`
+      let scale = - (lastScrollY / f ) + 2;
+      let y = 54 * (1 - lastScrollY / f);
+      let a = this.toolbar.clientWidth > 840 ? 40 : 15;
+      let x = a * (1 - lastScrollY / f);
+      this.title.style.transform = `scale(${scale}) translate(${x}%, ${y}px)`
 
       this.bigHeaderHidden = false;
     }else{
 
       if(!this.bigHeaderHidden){
-        title.style.transform = 'scale(1) translate(0,0)';
-        toolbar.style.boxShadow = `
+        this.title.style.transform = 'scale(1) translate(0,0)';
+        this.toolbar.style.boxShadow = `
         0 3px 4px 0 rgba(0, 0, 0, .14),
         0 3px 3px -2px rgba(0, 0, 0, .2),
         0 1px 8px 0 rgba(0, 0, 0, .12)`
       }
 
       this.bigHeaderHidden = true
-
     }
-
   }
 
   render(){
@@ -55,7 +68,7 @@ export default class MainPanel extends React.Component{
       <div className={mainClasses}>
         <EventListener
           target={this.props.panelID}
-          onScroll={withOptions(this.handleScroll, {passive: true, capture: false})}/>
+          onScroll={this.handleScroll}/>
 
         {React.cloneElement(this.props.toolbar, { titleID: `${this.props.panelID}-ttbar` })}
 
