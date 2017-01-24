9d1ad0bc5f5091401db316d59070b8b489856810
DavidMBP <steadypathapp@gmail.com>
2017-01-23 19:24:31 -0600

    Started customer analytics

---
+++ imports/ui/models/customers/show/CustomerAnalytics.jsx
@@ -0,0 +1,26 @@
+
+/*
+Sells Graph
+  x-axis = time
+  y-axis = total price
+
+Sum of Total Spent
+
+Conditions
+  isPaid
+  status
+*/
+
+
+
+/*
+Product Graph
+  x-axis = time
+  y-axis = quantity
+
+Top 5 Products
+
+Conditions
+  hasBatch
+  sellIsPaid
+*/
